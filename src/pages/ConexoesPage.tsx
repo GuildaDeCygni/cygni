5199c29ea40a00535776213878d03a20c172860a
LeonardoTedeschi <Leonardo.olitedeschi@gmail.com>
2026-03-31 16:28:30 -0300

    Atualizado o video na conexões

--- src/pages/ConexoesPage.tsx
+++ src/pages/ConexoesPage.tsx
@@ -72,7 +72,7 @@ export function ConexoesPage() {
             </div>
             <div className="aspect-video w-full rounded-lg overflow-hidden border border-cream-muted/10 bg-void">
               <iframe
-              src="https://www.youtube.com/embed?v=telNZfxAYgo" 
+              src="https://www.youtube.com/embed/telNZfxAYgo"
                 title="YouTube video player"
                 frameBorder="0"
                 allowFullScreen
